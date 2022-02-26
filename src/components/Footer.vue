c9c2f0eb870969abd1168dd510fcf5568db371c4
Calvin Paulus Wijaya <45009720+calvinpa@users.noreply.github.com>
2022-02-27 01:42:51 +0900

    Add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,48 +1,43 @@
 <template>
   <v-footer
-    dark
     padless
   >
     <v-card
       class="flex"
-      flat
       tile
     >
-      <v-card-title class="teal">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-
-        <v-spacer></v-spacer>
-
+      <v-card-title class="justify-center">
         <v-btn
-          v-for="icon in icons"
-          :key="icon"
+          v-for="sns in socials"
+          :key="sns.icon"
           class="mx-4"
-          dark
           icon
+          :href="sns.href"
+          target="_blank"
         >
-          <v-icon size="24px">
-            {{ icon }}
+          <v-icon size="24px" color="grey">
+            {{ sns.icon }}
           </v-icon>
         </v-btn>
       </v-card-title>
 
-      <v-card-text class="py-2 white--text text-center">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      <v-card-text class="py-2 --text text-center">
+        {{ new Date().getFullYear() }} — <strong>{{profile.full_name}}</strong>
       </v-card-text>
     </v-card>
   </v-footer>
 </template>
 
 <script>
+import profile from '../assets/profile.json'
+
 export default {
   name: 'Footer',
 
   data: () => ({
-    icons: [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-linkedin',
-      'mdi-instagram'
+    profile: profile,
+    socials: [
+      { icon: 'mdi-linkedin', href: 'https://www.linkedin.com/in/calvin-paulus' }
     ]
   })
 }
